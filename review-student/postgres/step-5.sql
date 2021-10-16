b841483a198a1655ca3ca3f22e4ec79c5d708175
Pareesa Madjd <pareesa.madjd@gmail.com>
2021-10-16 10:36:08 -0400

    Last part finished!

--- review-student/postgres/step-5.sql
+++ review-student/postgres/step-5.sql
@@ -9,13 +9,16 @@
   description: Ohiopyle State Park is a Pennsylvania state park on 19,052 acres in Dunbar, Henry Clay and Stewart Townships, Fayette County, Pennsylvania in the United States. The focal point of the park is the more than 14 miles of the Youghiogheny River Gorge that passes through the park.
   ------------------------------
 */
-
+INSERT INTO park (name, location, establish_date, area, visitors, description)
+VALUES ('Ohiopyle State Park', 'Pennsylvania', '1965-01-01', 19052, 1000000, 'Ohiopyle State Park is a Pennsylvania state park on 19,052 acres in Dunbar, Henry Clay and Stewart Townships, Fayette County, Pennsylvania in the United States. The focal point of the park is the more than 14 miles of the Youghiogheny River Gorge that passes through the park.');
 
 /*
   STEP TWO: You just found out that there was an error with the park data. Please update the park visitors to 1.5 million anually.
 
 */
-
+UPDATE park
+SET visitors = 1500000
+WHERE park.name = 'Ohiopyle State Park';
 
 /*
  STEP THREE: Insert new campground with the following data:
@@ -27,6 +30,8 @@
   daily_fee: 95.00
   ------------------------------------------------------------
 */
+INSERT INTO campground(park_id, name, open_from_mm, open_to_mm, daily_fee)
+VALUES (4, 'Youghiogheny', 01, 12, 95.00);
 
 
 /*
@@ -41,6 +46,8 @@
 
 */
 
+INSERT INTO site (site_number, campground_id)
+VALUES (623, 8), (624, 8), (625, 8);
 
 /*
  STEP FIVE: Insert 3 reservations, 1 for each site with the following data:
@@ -51,16 +58,21 @@
 ------------------------------------------------------------------------------------
 
 */
-
+INSERT INTO reservation (site_id, name, from_date, to_date)
+VALUES (623, 'Wayne Family', '2021-10-26', '2021-11-05'), (624, 'Parker Family', '2021-10-27', '2021-11-05'), (625, 'Kent Family', '2021-10-28', '2021-11-05');
 
 /*
  STEP SIX: The Wayne Family called and asked if they could change their reservation to today. Update the from_date to today and the to_date to a week from today.
 
  */
-
+UPDATE reservation
+SET from_date = '2021-10-16',
+    to_date = '2021-10-23'
+WHERE reservation_id = 50;
 
 /*
  STEP SEVEN: The Kent family called and they would like to cancel their reservation. Delete the reservation for Kent Family.
 
 */
-
+DELETE FROM reservation
+WHERE reservation_id = 52;
