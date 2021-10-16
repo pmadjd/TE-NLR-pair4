129babf28e7896e68b59771e66c783cb5f8b57de
Charlotte Kies <chopekies@gmail.com>
2021-10-15 22:05:18 -0400

    Charlotte's second editions

--- review-student/postgres/step-4.sql
+++ review-student/postgres/step-4.sql
@@ -1,14 +1,30 @@
 -- select the park name, campground name, open_from_mm, open_to_mm & daily_fee ordered by park name and then campground name
+SELECT park.name, campground.name, open_from_mm, open_to_mm, daily_fee
+FROM campground
+LEFT JOIN park ON campground.park_id = park.park_id
+ORDER BY park.name, campground.name;
 
 
 -- select the park name and the total number of campgrounds for each park ordered by park name
+SELECT park.name, COUNT(campground.name) 
+FROM park
+LEFT JOIN campground ON campground.park_id = park.park_id
+GROUP BY park.name
+ORDER BY park.name;
 
 
 
 -- select the park name, campground name, site number, max occupancy, accessible, max rv length, utilities where the campground name is 'Blackwoods'
+SELECT park.name, campground.name, site_number, max_occupancy, accessible, max_rv_length, utilities
+FROM park
+JOIN campground ON campground.park_id = park.park_id
+JOIN site ON site.campground_id = campground.campground_id
+WHERE campground.name = 'Blackwoods';
+
 
 /*
-  select park name, campground, total number of sites (column alias 'number_of_sites') ordered by park
+  select park name, campground, total number of sites (column alias 'number_of_sites') ordered by park 
+  
   -------------------------------------------------
     Acadia	Blackwoods	276
     Acadia	Seawall	198
@@ -20,9 +36,20 @@
   -------------------------------------------------
 */
 
+SELECT park.name, campground.name, COUNT(site.site_id) AS number_of_sites
+FROM park
+JOIN campground ON campground.park_id = park.park_id
+JOIN site ON site.campground_id = campground.campground_id
+GROUP BY park.name, campground.name
+ORDER BY park.name, number_of_sites DESC;
+--Question does not indicate to sort second by number_of_sites, 
+--but the example is sorted by name, then number_of_sites, so that's what we did to get the same table as provided above. 
 
 -- select site number, reservation name, reservation from and to date ordered by reservation from date
-
+SELECT site_number, reservation.name, reservation.from_date, reservation.to_date
+FROM site
+JOIN reservation ON reservation.site_id = site.site_id
+ORDER BY reservation.from_date;
 
 
 /*
@@ -38,6 +65,12 @@
   -------------------------------------------------
 */
 
+SELECT campground.name, COUNT(reservation.reservation_id)
+FROM campground
+JOIN site ON site.campground_id = campground.campground_id
+JOIN reservation ON site.site_id = reservation.site_id
+GROUP BY campground.name
+ORDER BY COUNT(reservation.reservation_id) DESC;
 
 
 
